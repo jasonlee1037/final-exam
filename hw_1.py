c282e8ec3f434afc3c39640d7633abccb52da213
Jason Lee <jasonlee1037@gmail.com>
2015-03-20 09:02:15 +0000

    add log

--- hw_1.py
+++ hw_1.py
@@ -1,8 +1,12 @@
 import glob
 import os
+import datetime
 # part 1, open + read all .seq files
 path = "fasta_problem"
+name = []
+length = []
 for filename in glob.glob(os.path.join(path, '*.seq')):
+	count = 0
 	f = open(filename, "r")
 	# part 2, single fasta-formatted file per individual file
 	new_filename = filename.strip(".seq") + ".fas"
@@ -10,9 +14,24 @@ for filename in glob.glob(os.path.join(path, '*.seq')):
 	for i, line in enumerate (f):
 		if i == 0:
 			new_file.write(line)
+			#extract name for part 4
+			name.append(line.strip(">").strip("\n"))
 		#part 3
 		else:
 			new_file.write(line.lower())
+			#get length of sequence
+			count += len(line)
+	length.append(count)
 	new_file.close()
 	f.close()
+
+#part 4 make log file
+log_name = "log.txt"
+f = open(log_name, "w")
+f.write("sequence_name,length\n")
+for i in range(10):
+	f.write(name[i] + "," + str(length[i]) + "\n")
+#part 5 date and time
+current_time = datetime.datetime.now()
+f.write(str(current_time))
 
